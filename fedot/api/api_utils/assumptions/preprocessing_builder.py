c7939ad2a2d11bf5c2147bda84d9aefbb389b991
andreygetmanov <andrey.getmanov2802@gmail.com>
2022-04-30 13:56:32 +0300

    - improved data_has_text_features function

--- fedot/api/api_utils/assumptions/preprocessing_builder.py
+++ fedot/api/api_utils/assumptions/preprocessing_builder.py
@@ -29,7 +29,7 @@ class PreprocessingBuilder:
         if data_has_categorical_features(data):
             preprocessing_builder = preprocessing_builder.with_categorical()
         if data_has_text_features(data):
-            preprocessing_builder = preprocessing_builder.with_text()
+            preprocessing_builder = preprocessing_builder.with_text_vectorizer()
         if data_has_image_features(data):
             preprocessing_builder = preprocessing_builder.with_image()
         return preprocessing_builder.to_builder()
@@ -48,7 +48,7 @@ class PreprocessingBuilder:
             self._builder.add_node('scaling')
         return self
 
-    def with_text(self):
+    def with_text_vectorizer(self):
         if self.task_type != TaskTypesEnum.ts_forecasting:
             self._builder.add_node('tfidf')
         return self
